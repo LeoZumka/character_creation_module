9d4262119b1922a5ff5ffb1a7fa3338a59c958ad
LeoZumka <leozumka@gmail.com>
2022-12-15 22:12:02 +0500

    Fix annotations str None

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def start_training(char_name: str, char_class: str) -> str:
           '— чтобы блокировать атаку противника или special'
           '— чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = None
+    cmd: str = ''
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -60,8 +60,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    approve_choice: str = None
-    char_class: str = None
+    approve_choice: str = ""
+    char_class: str = ""
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого'
                            'хочешь играть:'
